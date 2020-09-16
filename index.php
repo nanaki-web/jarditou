8d7ca375006b67a943e5b13726f1ea2b4cdde4e7
anne <anne.wilga@orange.fr>
2020-09-16 16:52:37 +0200

    bibliotheque de fonctions

--- index.php
+++ index.php
@@ -1,62 +1,10 @@
-<!-- <!doctype html> -->
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Jardintou accueil</title>
-</head>
-<body>
-    <!-- <div class="container-fluid"> 
-        <div class="row">
-            <div class="col-12 col-sm-12 col-md-12 col-lg-8"></div> 
-            <div class="col-12 col-sm-12 col-md-12 col-lg-4"></div>
-        </div> -->
-          
-<!-- logo+tout jardin -->
-            <div class="row">
-                <div class="col-12 col-md-6 "> 
-                    <img src="public/images/logo/jarditou_logo2.png" class="img-fluid" alt="Image responsive">
-                </div>
-                <div class="col-12 col-md-6">      
-                  <h1 class="text-right mt-3"><strong>Tout le jardin</strong></h1>
-                </div> 
-            </div>
-<!-- navigation -->
-        
-           
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Jarditou.com</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="index.php">Accueil </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="tableau.php">Tableau</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="contact.php">contact</a>                    
-                    </li>
-                  </ul>
-                  <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Votre promotion" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-                  </form>
-                </div>
-              </nav>
-            <!--banniere promotion  -->
-            <div class="row">
-                <div class="col-12 col-md-12" >
-                <img   src="public/images/logo/promotion.jpg" class="img-fluid w-100 " alt="Image responsive">
-                </div>
-            </div>
+
+<?php
+include("header.php");
+
+
+
+?>
 <!-- texte -->
             <div class="row">
                 <div class="col-12 col-md-8">
@@ -77,78 +25,6 @@
             
             </div>
 
-            <div class="row">
-                <div class="col-12 col-md-12">
-                <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-                    <!-- navigation-->
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>   
-                    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php">Mentions légales</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="tableau.php">Horaires</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="formulaire.php">Plan du site</a>
-                            </li>
-                        </ul>               
-                    </div> 
-                </div>
-            </nav>
-            </div>  
-                  
-                  
-
-           
-
-            
-              
-
-
-
-
-
-
-
-
-
-
-    </div>
-
-
-
-
-    
-    
-    
-   
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-    
-
-
-<!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
-</body>
-</html>
+<?php 
+    include("footer.php");
+?> 
