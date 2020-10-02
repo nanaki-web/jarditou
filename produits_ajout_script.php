6b26fed361bbac12d3cee3c7d3c2698a553005f1
Anne Wilga <anne.wilga@orange.fr>
2020-10-02 17:14:43 +0200

    modification page ajout script

--- produits_ajout_script.php
+++ produits_ajout_script.php
@@ -3,69 +3,46 @@
 require "connexion_bdd.php"; // Inclusion de notre bibliothèque de fonctions
 $db = connexionBase(); // Appel de la fonction de connexion
 // declaration des variables
-$reference = $_POST["reference"];//1
-$categorie = $_POST["categorie"];//2
-$libelle = $_POST["libelle"];//3
-$description = $_POST["description"];//4
-$prix = $_POST["prix"];//5
-$stock = $_POST["stock"];//6
-$couleur = $_POST["couleur"];//7
-$photo = $_POST["photo"];//8
-$dateAjout = $_POST["dateAjout"];//9
-$bloque = $_POST["bloque"];//10
+$reference = valid_donnees($_POST["reference"]);//1
+$categorie = valid_donnees($_POST["categorie"]);//2
+$libelle = valid_donnees($_POST["libelle"]);//3
+$description = valid_donnees($_POST["description"]);//4
+$prix = valid_donnees($_POST["prix"]);//5
+$stock = valid_donnees($_POST["stock"]);//6
+$couleur = valid_donnees($_POST["couleur"]);//7
+$photo = valid_donnees($_POST["photo"]);//8
+$dateAjout = valid_donnees($_POST["dateAjout"]);//9
+$bloque = valid_donnees($_POST["bloque"]);//10
+
+
+function valid_donnees($donnees)
+{
+    $donnees = trim($donnees); // trim == supprimer les espaces inutiles
+    $donnees = stripslashes($donnees); //stripslashes == supprimer les antislashes 
+    $donnees = htmlspecialchars($donnees); //htmlspecialchars == échapper certains caractères spéciaux comme les chevrons « < » et « > » en les transformant en entités HTML
+    return $donnees;
+}
+
+
+
 
 //verification
-if (isset ($_POST ["pro_ref"]) && (isset ($_POST ["pro_cat_id"]) && isset($_POST["pro_libelle"]) && isset($_POST["pro_description"]) && isset($_POST["pro_prix"]) && isset($_POST["pro_stock"]) && isset($_POST["pro_couleur"]) && isset($_POST["pro_photo"])&& isset($_POST["pro_d_ajout"])&& isset($_POST["pro_bloque"])
-&& !empty($_POST["pro_ref"]) && !empty($_POST["pro_cat_id"]) && !empty($_POST["pro_libelle"])&& !empty($_POST["pro_description"]) && !empty($_POST["pro_prix"])&& !empty($_POST["pro_stock"])&& !empty($_POST["pro_couleur"])&& !empty($_POST["pro_photo"])&& !empty($_POST["pro_d_ajout"])&& !empty($_POST["pro_bloque"]))
-{
-    $d = $_POST ;
+// if (isset ($_POST ["pro_ref"]) && (isset ($_POST ["pro_cat_id"]) && isset($_POST["pro_libelle"]) && isset($_POST["pro_description"]) && isset($_POST["pro_prix"]) && isset($_POST["pro_stock"]) && isset($_POST["pro_couleur"]) && isset($_POST["pro_photo"])&& isset($_POST["pro_d_ajout"])&& isset($_POST["pro_bloque"])
+// && !empty($_POST["pro_ref"]) && !empty($_POST["pro_cat_id"]) && !empty($_POST["pro_libelle"])&& !empty($_POST["pro_description"]) && !empty($_POST["pro_prix"])&& !empty($_POST["pro_stock"])&& !empty($_POST["pro_couleur"])&& !empty($_POST["pro_photo"])&& !empty($_POST["pro_d_ajout"])&& !empty($_POST["pro_bloque"]))
 
-    switch (0) 
-    {
-        case strlen(trim($d["pro_ref"])):
-            $error = true ;
-            $erreur["type"] = "alert";
-            $erreur["content"] = "le champs référence est obligatoire";
-            break;
 
-        case strlen(trim($d["pro_cat_id"])):
-            $error = true ;
-            $erreur["type"] = "alert";
-            $erreur["content"] = "le champs catégorie est obligatoire";
-            break;
 
-        case strlen(trim($d["pro_libelle"])):
-            $error = true ;
-            $erreur["type"] = "alert";
-            $erreur["content"] = "le champs libéllé est obligatoire";
-            break;
-                
-        case strlen(trim($d["pro_prix"])):
-            $error = true ;
-            $erreur["type"] = "alert";
-            $erreur["content"] = "le champs prix est obligatoire";
-            break;
-                    
-        case strlen(trim($d["pro_stock"])):
-            $error = true ;
-            $erreur["type"] = "alert";
-            $erreur["content"] = "le champs stock est obligatoire";
-            break;
-        
-        case strlen(trim($d["pro_d_ajout"])):
-            $error = true ;
-            $erreur["type"] = "alert";
-            $erreur["content"] = "le champs date d'ajout est obligatoire";
-            break;
-        
-       
-    }
 
-if (isset($erreur))
-{
-    echo '<div id="notification" class='.$erreur["type].'">'.$erreur["content"].'</div>;
-}
-}
+
+//if (isset ($_POST ["pro_ref"])
+//&& (!empty ($_POST ["pro_ref"]) 
+//&& preg_match("^[A-Za-z '-]+$",$reference)
+//{
+ //   $error['reference'] = 'la référence est obligatoire ';
+//}
+
+
+
 // preparation de la requete d'insertion
 $pdoStat = $db->prepare('INSERT INTO produits(pro_cat_id, pro_ref, pro_libelle, pro_description, pro_prix, pro_stock, pro_couleur,pro_photo , pro_d_ajout,pro_bloque )   
                         VALUES(:categorie,:reference,:libelle,:descrip,:prix,:stock,:couleur,:photo,:dateAjout,:produit_bloque)'); // ici ton :description a un s
@@ -88,13 +65,15 @@ $pdoStat->bindParam(':produit_bloque', $bloque);
 
 $pdoStat->execute();
 
-// if (isset ($_POST ["pro_ref"]) && isset($_POST["pro_libelle"]) && isset($_POST["pro_description"]) && isset($_POST["pro_prix"]) && isset($_POST["pro_stock"]) && isset($_POST["pro_couleur"]) && isset($_POST["pro_photo"])&& isset($_POST["pro_d_ajout"])&& isset($_POST["pro_bloque"])
-// && !empty($_POST["pro_ref"]) && !empty($_POST["pro_libelle"])&& !empty($_POST["pro_description"]) && !empty($_POST["pro_prix"])&& !empty($_POST["pro_stock"])&& !empty($_POST["pro_couleur"])&& !empty($_POST["pro_photo"])&& !empty($_POST["pro_d_ajout"])&& !empty($_POST["pro_bloque"]))
-// {
 
-
+//Redirection vers le tableau
+header("Location:tableau.php");
+ 
+    } 
+}
 
 
 
  
 var_dump($_POST); 
+?>
